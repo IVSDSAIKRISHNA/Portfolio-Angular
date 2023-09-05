ab7aed4c2b9babf2f5914b33449d8fe20e83ec3c
unknown <ivnsainitin@gmail.com>
2023-09-06 00:57:50 +0530

    Initial Commit

---
+++ Portfolio-Angular/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'header', pathMatch:'full'},
+  {path:'header',component:HeaderComponent},
+  {path:'footer',component:FooterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
